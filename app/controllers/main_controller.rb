b8c280a0491f58b009f4fe43179b6eb1c7fadd0f
haru69 <haruki.footboll.boy@gmail.com>
2020-09-20 23:30:35 +0900

    add env gitignore

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,18 +1,43 @@
 class MainController < ApplicationController
+  require 'net/http'
+  require 'uri'
+  require 'json'
+
   def index
-    require 'net/http'
-    require 'uri'
-    require 'json'
-    uri = URI.parse('http://webservice.recruit.co.jp/hotpepper/gourmet/v1/?key=a53d2b44f8d80d6d&address=#{@address}&range=#{@range}&non_smoking=#{@smoking}&format=json')
+
+    uri = URI.parse URI.encode('http://webservice.recruit.co.jp/hotpepper/gourmet/v1/?key=a53d2b44f8d80d6d&address=#{@address}&range=#{@range}&non_smoking=#{@smoking}&format=json')
     
-    json = Net::HTTP.get(uri)
-    result = JSON.parse(json)
+    response = JSON.load(Net::HTTP.get(uri))
+      response["rest"]
     
-    @address = 1
-    @range = 1
-    @budget = 'http://webservice.recruit.co.jp/hotpepper/budget/v1/?key=a53d2b44f8d80d6d&format=json'
-    @smoking = 1 
+    @response =  response["rest"]
   end
+
+def update
+  @lat = params[:lat]
+  @lng = params[:lng]
+  @range = params[:range]
+  @maney = params[:maney]
+  @smoking = params[:smoking]
+  
+  data = {
+    "key": ENV['API_KEY'],
+    "lat": @lat,
+    "lng": @lng,
+    "range": @range,
+    "budget": @maney,
+    "non_smoking": @smoking,
+    "format": "json"
+  }
+
+  query = data.to_query
+    uri = URI.parse("http://webservice.recruit.co.jp/hotpepper/gourmet/v1/?" + query)
+    res = Net::HTTP.get_response(uri)
+    res_data = JSON.parse(res.body)
+
+    results = res_data["results"]["shop"]
+
+end
 
   def show
   end
