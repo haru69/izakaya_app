acd2b783f3ccda39d2c56a3dae93b692e4a7fb33
haru69 <haruki.footboll.boy@gmail.com>
2020-09-29 19:10:18 +0900

    responce OK

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -5,35 +5,56 @@ class MainController < ApplicationController
 
   def index
 
-    @lat = session[:lat]
-    @lng = session[:lng]
+   # @lat = params[:lat]
+    #@lng = params[:lng]
+    #@range = params[:range]
+    #@maney = params[:maney]
+    #@smoking = params[:smoking]
+    
+    data = {
+      "key": "a53d2b44f8d80d6d",
+      "lat": 35.531328,
+      "lng": 139.696899,
+      "range": 1,
+      #"budget": 1,
+      "non_smoking": 0,
+      "format": "json"
+    }
+  
+    query = data.to_query
+      uri = URI.parse('http://webservice.recruit.co.jp/hotpepper/gourmet/v1/?' + query )
+      res = Net::HTTP.get_response(uri)
+      res_data = JSON.parse(res.body)
+     @results = res_data
   end
 
 def update
 
- 
+  require 'net/http'
+  require 'uri'
+  require 'json'
 
-  @lat = results[0].geometry.location.lat
-  @lng = "1"
+  @lat = results[0].geometry.location.lat()
+  @lng = results[0].geometry.location.lng()
   @range = params[:range]
   @maney = params[:maney]
   @smoking = params[:smoking]
   
   data = {
     "key": ENV['API_KEY'],
-    "lat": @lat,
-    "lng": @lng,
-    "range": @range,
-    "budget": @maney,
-    "non_smoking": @smoking,
+    "lat": 35.531328,
+    "lng": 139.696899,
+    "range": 1,
+    "budget": 1,
+    "non_smoking": 1,
     "format": "json"
   }
 
   query = data.to_query
-    uri = URI.parse("http://webservice.recruit.co.jp/hotpepper/gourmet/v1/?" + query)
+    uri = URI.parse('http://webservice.recruit.co.jp/hotpepper/gourmet/v1/?key=a53d2b44f8d80d6d&lat=35.531328&lng=139.696899&range=2&non_smoking=1&format=json')
     res = Net::HTTP.get_response(uri)
-    res_data = JSON.parse(res.body)
-    results = res_data["results"]["shop"]
+   res_data = JSON.parse(res.body)
+   @results = res_data
 
 end
 
@@ -42,6 +63,34 @@ end
     @lng = 1
   end
 
- 
+ # 200 Success
+def response_success(class_name, action_name)
+  render status: 200, json: { status: 200, message: "Success #{class_name.capitalize} #{action_name.capitalize}" }
+end
+
+# 400 Bad Request
+def response_bad_request
+  render status: 400, json: { status: 400, message: 'Bad Request' }
+end
+
+# 401 Unauthorized
+def response_unauthorized
+  render status: 401, json: { status: 401, message: 'Unauthorized' }
+end
+
+# 404 Not Found
+def response_not_found(class_name = 'page')
+  render status: 404, json: { status: 404, message: "#{class_name.capitalize} Not Found" }
+end
+
+# 409 Conflict
+def response_conflict(class_name)
+  render status: 409, json: { status: 409, message: "#{class_name.capitalize} Conflict" }
+end
+
+# 500 Internal Server Error
+def response_internal_server_error
+  render status: 500, json: { status: 500, message: 'Internal Server Error' }
+end
   
 end
